9a990ebcb27fa5ffdf92e8d95d6c7462a79510c1
Joakim Vrangum <joakimvrangum@gmail.com>
2020-12-28 19:16:08 +0100

    Switched to rollup-build

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueFlipFlopCard from '@/vue-flip-flop-card.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueFlipFlopCard
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-flip-flop-card />
+  </div>
+</template>
