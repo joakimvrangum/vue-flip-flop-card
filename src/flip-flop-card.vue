930453d037f4b389bff24d44ccad313c02f41f8d
Joakim Vrangum <joakimvrangum@gmail.com>
2021-01-03 19:01:02 +0100

    Added hexToRGB method, and continued work on styled (front) card

--- src/flip-flop-card.vue
+++ src/flip-flop-card.vue
@@ -12,9 +12,10 @@
 
       <div v-else class="styled">
         <div class="styled__image" :style="styledFrontImage"></div>
-        <h4 class="styled__heading">
-          <span class="styled__heading-span">Styled heading</span>
+        <h4 v-if="styledFront.coverTitle" class="styled__title">
+          <span class="styled__title-span" :style="styledFrontTitle">{{styledFront.coverTitle}}</span>
         </h4>
+        <slot name="styled-content"></slot>
       </div>
     </div>
 
@@ -74,8 +75,23 @@ export default {
       return `0 0 12px rgba(173, 173, 173, ${(this.elevation / 10)})`;
     },
     styledFrontImage() {
-      return `background-image: linear-gradient(to right, ${this.styledFront.themeColor || 'transparent'}, ${this.styledFront.themeColor2  || 'transparent'}), url(${this.styledFront.headerImage});`
+      return `background-image: linear-gradient(to right, ${this.styledFront.themeColor || 'transparent'}, ${this.styledFront.themeColor2  || 'transparent'}), url(${this.styledFront.coverImg});`
           + `height: ${this.height * 0.35}px;`;
+    },
+    styledFrontTitle() {
+      return `background-image: linear-gradient(to right bottom, ${this.convertHexToRGB(this.styledFront.themeColor)}, ${this.convertHexToRGB(this.styledFront.themeColor2)});`;
+    }
+  },
+  methods: {
+    convertHexToRGB: function (hex, alpha=0.87) {
+        let r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+        if (alpha) {
+          return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+        } else {
+          return "rgb(" + r + ", " + g + ", " + b + ")";
+        }
     }
   }
 }
@@ -152,6 +168,21 @@ export default {
       background-size: cover;
       background-blend-mode: screen;
       clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
+    }
+    &__title {
+      font-size: 1.5em;
+      font-weight: 600;
+      text-transform: uppercase;
+      text-align: right;
+      position: absolute;
+      top: 30%;
+      right: 0;
+
+      &-span {
+        padding: 0.7rem 1rem;
+        box-decoration-break: clone;
+        -webkit-box-decoration-break: clone;
+      }
     }
   }
 }
