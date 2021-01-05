ea5b7ec64841666a714baa606f4e9cfacd27b448
Joakim Vrangum <joakimvrangum@gmail.com>
2021-01-05 23:25:41 +0100

    Started refactoring styled-card

---
+++ src/flip-flop-card-styled.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="styled">
+    <div class="styled__image" :style="styledImage"></div>
+    <h4 v-if="styleObj.coverTitle" class="styled__title">
+      <span class="styled__title-span" :style="styledTitle">{{ styleObj.coverTitle }}</span>
+    </h4>
+    <div class="styled__content">
+      <slot name="styled-front-content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    styleObj: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    styledImage() {
+      return `background-image: linear-gradient(to right, ${this.styleObj.themeColor || 'transparent'}, ${this.styleObj.themeColor2 || 'transparent'}), url(${this.styleObj.coverImg});`
+          + `height: ${400 * 0.35}px;`;
+    },
+    styledTitle() {
+      return `background-image: linear-gradient(to right bottom, ${this.convertHexToRGB(this.styleObj.themeColor)}, ${this.convertHexToRGB(this.styleObj.themeColor2)});`;
+    },
+  },
+  methods: {
+    convertHexToRGB: function (hex, alpha = 0.87) {
+      let r = parseInt(hex.slice(1, 3), 16),
+          g = parseInt(hex.slice(3, 5), 16),
+          b = parseInt(hex.slice(5, 7), 16);
+      if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+      } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.styled {
+  &__image {
+    background-size: cover;
+    background-blend-mode: screen;
+    clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
+  }
+
+  &__title {
+    font-size: 1.5em;
+    font-weight: 600;
+    text-transform: uppercase;
+    text-align: right;
+    position: absolute;
+    top: 30%;
+    right: 0;
+
+    &-span {
+      padding: 0.7rem 1rem;
+      box-decoration-break: clone;
+      -webkit-box-decoration-break: clone;
+    }
+  }
+
+  &__content {
+    position: absolute;
+    top: 44%;
+  }
+}
+</style>
--- src/flip-flop-card.vue
+++ src/flip-flop-card.vue
@@ -9,16 +9,7 @@
         :style="{'transition': transformSpeed, 'box-shadow': elevationEffect}"
     >
       <slot v-if="!styledFront" name="front"></slot>
-
-      <div v-else class="styled">
-        <div class="styled__image" :style="styledFrontImage"></div>
-        <h4 v-if="styledFront.coverTitle" class="styled__title">
-          <span class="styled__title-span" :style="styledFrontTitle">{{styledFront.coverTitle}}</span>
-        </h4>
-        <div class="styled__content">
-          <slot name="styled-front-content"></slot>
-        </div>
-      </div>
+      <styled-card v-else :styleObj="styledFront"></styled-card>
     </div>
 
     <div
@@ -27,23 +18,16 @@
         :style="{'transition': transformSpeed, 'box-shadow': elevationEffect}"
     >
       <slot v-if="!styledBack" name="back"></slot>
-
-      <div v-else class="styled">
-        <div class="styled__image" :style="styledBackImage"></div>
-        <h4 v-if="styledBack.coverTitle" class="styled__title">
-          <span class="styled__title-span" :style="styledBackTitle">{{styledBack.coverTitle}}</span>
-        </h4>
-        <div class="styled__content">
-          <slot name="styled-back-content"></slot>
-        </div>
-      </div>
+      <styled-card v-else :styleObj="styledBack"></styled-card>
       </div>
   </div>
 </template>
 
 <script>
+import StyledCard from "./flip-flop-card-styled";
 export default {
   name: 'FlipFlopCard',
+  components: {StyledCard},
   props: {
     /**
      * Height of the card in pixels
@@ -109,32 +93,6 @@ export default {
     },
     elevationEffect() {
       return `0 0 12px rgba(173, 173, 173, ${(this.elevation / 10)})`;
-    },
-    styledFrontImage() {
-      return `background-image: linear-gradient(to right, ${this.styledFront.themeColor || 'transparent'}, ${this.styledFront.themeColor2  || 'transparent'}), url(${this.styledFront.coverImg});`
-          + `height: ${this.height * 0.35}px;`;
-    },
-    styledFrontTitle() {
-      return `background-image: linear-gradient(to right bottom, ${this.convertHexToRGB(this.styledFront.themeColor)}, ${this.convertHexToRGB(this.styledFront.themeColor2)});`;
-    },
-    styledBackImage() {
-      return `background-image: linear-gradient(to right, ${this.styledBack.themeColor || 'transparent'}, ${this.styledBack.themeColor2  || 'transparent'}), url(${this.styledBack.coverImg});`
-          + `height: ${this.height * 0.35}px;`;
-    },
-    styledBackTitle() {
-      return `background-image: linear-gradient(to right bottom, ${this.convertHexToRGB(this.styledBack.themeColor)}, ${this.convertHexToRGB(this.styledBack.themeColor2)});`;
-    }
-  },
-  methods: {
-    convertHexToRGB: function (hex, alpha=0.87) {
-        let r = parseInt(hex.slice(1, 3), 16),
-            g = parseInt(hex.slice(3, 5), 16),
-            b = parseInt(hex.slice(5, 7), 16);
-        if (alpha) {
-          return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-        } else {
-          return "rgb(" + r + ", " + g + ", " + b + ")";
-        }
     }
   }
 }
@@ -203,32 +161,6 @@ export default {
       transform: rotateX(0deg);
       -moz-transform: rotateX(0deg);
       -webkit-transform: rotateX(0deg);
-    }
-  }
-
-  .styled {
-    &__image {
-      background-size: cover;
-      background-blend-mode: screen;
-      clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
-    }
-    &__title {
-      font-size: 1.5em;
-      font-weight: 600;
-      text-transform: uppercase;
-      text-align: right;
-      position: absolute;
-      top: 30%;
-      right: 0;
-      &-span {
-        padding: 0.7rem 1rem;
-        box-decoration-break: clone;
-        -webkit-box-decoration-break: clone;
-      }
-    }
-    &__content {
-      position: absolute;
-      top: 44%;
     }
   }
 }
